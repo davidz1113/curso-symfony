2a4c44b5641dbed55c77d2f16a1abd2d79a92449
David Andres <d.carvajal_13@hotmail.com>
2018-08-15 14:28:29 -0500

    manejando base de datos

---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use AppBundle\Services\Helpers;
+
+class DefaultController extends Controller
+{
+   
+    public function indexAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/index.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
+
+    public function pruebasAction(){
+        $em = $this->getDoctrine()->getManager();
+        $userRepo= $em->getRepository('BackendBundle:User');
+        $users=$userRepo->findAll();
+        
+        $helpers = $this->get(Helpers::class);
+        return $helpers->json(array
+        (
+            'status' => 'success',
+            'users' => $users[1]
+        
+        ));
+        /*
+        die();
+
+        return new JsonResponse(array
+            (
+                'status' => 'success',
+                'users' => $users[0]->getName()
+            
+            ));
+            */
+    }
+}
