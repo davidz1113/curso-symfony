9ee64fe3a008bb860c7f1cbf0eb3dba6a16acd94
David Andres <d.carvajal_13@hotmail.com>
2018-08-16 08:22:32 -0500

    Rest Api

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -7,29 +7,75 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use AppBundle\Services\Helpers;
+use Symfony\Component\Validator\Constraints as Assert;
 
 class DefaultController extends Controller
 {
-   
+
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')) . DIRECTORY_SEPARATOR,
         ]);
     }
 
-    public function pruebasAction(){
-        $em = $this->getDoctrine()->getManager();
-        $userRepo= $em->getRepository('BackendBundle:User');
-        $users=$userRepo->findAll();
-        
+    //Login
+
+    public function loginAction(Request $request)
+    {
         $helpers = $this->get(Helpers::class);
-        return $helpers->json(array
-        (
+
+        //recibir Json por Post
+        $json = $request->get('json', null);
+        //array para devolver por defecto
+        $data = array(
+            'status' => 'error',
+            'data' => 'Send Json via post'
+        );
+        if ($json != null) {
+                //pasamos al login
+                //convertimos el json en un objeto de php
+            $params = json_decode($json);
+
+            $email = (isset($params->email)) ? $params->email : null;
+            $password = (isset($params->password)) ? $params->password : null;
+
+            $emailConstraint = new Assert\Email();
+            $emailConstraint->message = "This email is not valid";
+            $validate_email = $this->get("validator")->validate($email, $emailConstraint);
+
+            if ($email != null && count($validate_email) == 0 && $password != null) {
+
+                $data = array(
+                    'status' => 'success',
+                    'data' => 'Login success'
+                );
+            } else {
+                $data = array(
+                    'status' => 'error',
+                    'data' => 'Email or Password Incorrect'
+                );
+
+            }
+
+        }
+
+        return $helpers->json($data);
+    }
+    //fin login
+
+    public function pruebasAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $userRepo = $em->getRepository('BackendBundle:User');
+        $users = $userRepo->findAll();
+
+        $helpers = $this->get(Helpers::class);
+        return $helpers->json(array(
             'status' => 'success',
             'users' => $users[1]
-        
+
         ));
         /*
         die();
@@ -40,6 +86,6 @@ class DefaultController extends Controller
                 'users' => $users[0]->getName()
             
             ));
-            */
+         */
     }
 }
